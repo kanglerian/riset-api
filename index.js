268c2e5a5f28d572aea39c8b0806e460a19dfc9d
kampusinformatika <kanglerian.business@gmail.com>
2022-05-31 11:26:21 +0700

    cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require('cors');
+app.use(cors());
+
 const anggota = [
     {
         username: "kanglerian",
